22bf5ab39b321fbf3385037cbe65d0fbb7f78874
Ilia <95166972+ILIAHHne63@users.noreply.github.com>
2025-04-05 01:27:21 +0300

    Update checkpoints.sh

--- checkpoints.sh
+++ checkpoints.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 git lfs clone https://huggingface.co/xtuner/llava-llama-3-8b-v1_1-transformers AugModel/models/checkpoints/llava-llama-3-8b
-wget "https://drive.google.com/file/d/1e83wWQh9Tsficx0HM2rmc2KyYfBMpumf/view" -O clip_b16_grit1m_fultune_8xe.pth AugModel/models/checkpoints/
