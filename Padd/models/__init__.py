8b1fd1da0fb76ee623a2b8f2591b9d6ceb21d461
MarKovka20 <kovaleva.md@phystech.edu>
2025-04-05 00:45:31 +0300

    sec comm

--- Padd/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .PowerPaintControlNet import PowerPaintControlNet
-from .PhysicModel.physic_model import PhysicsModel
-
-__all__ = ["PowerPaintControlNet", "PhysicsModel"]
--- alphaCLIP_filter/preproc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/SunzeY/AlphaCLIP
-pip install -e AlphaCLIP/
-pip install loralib
-wget https://download.openxlab.org.cn/models/SunzeY/AlphaCLIP/weight/clip_b16_grit1m_fultune_8xe.pth
